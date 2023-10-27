294a85ff235cc07215239d9e244a34b9b6674010
Soyeon Park <aqua343@gmail.com>
2023-10-27 19:37:51 +0900

    메인화면 변경

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -3,9 +3,9 @@ import '../styles/main.css'
 const Main = () => {
     return (
         <>
-
-            <div className="dropBox">
-            <h1> Drop! </h1>
+            <div className="textBox">
+                <input type="text" className="text1" />
+                <button>입력</button>
             </div>
 
 
