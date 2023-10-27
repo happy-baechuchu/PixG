b17a7f4a1db7e3e66a7e32c4916370bd8396a301
Soyeon Park <aqua343@gmail.com>
2023-10-27 15:34:34 +0900

    환경설정

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  const msg = "안녕하세요! 상모만 입니더"
+  return (
+    <>
+      <div>
+        <h1>{msg}</h1>
+      </div>
+
+    </>
+  )
+}
+
+export default App
