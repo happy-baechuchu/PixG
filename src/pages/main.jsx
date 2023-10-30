c935b1c5697bc740cda958564c7e904270a2fd90
Soyeon Park <aqua343@gmail.com>
2023-10-30 19:56:04 +0900

    button 생성

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -3,9 +3,18 @@ import '../styles/main.css'
 const Main = () => {
     return (
         <>
-            <div className="textBox">
-                <input type="text" className="text1" />
-                <button>입력</button>
+            <form className="textBox" method="get">
+                <input type="text" className="text1" 
+                placeholder="English please." />
+                <button className="gen">Generate
+                </button>
+            </form>
+
+            <div className="outPut">
+                <button className="out"
+                a href=""
+                target="_blank"
+                rel="noopener noreferrer">Show me</button>
             </div>
 
 
