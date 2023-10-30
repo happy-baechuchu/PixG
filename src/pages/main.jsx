bf923906011f4428d9e465a1302562ea8805f32d
Soyeon Park <aqua343@gmail.com>
2023-10-30 22:43:39 +0900

    위치변경

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,26 +1,35 @@
-import '../styles/main.css';
-import { Link } from 'react-router-dom';
+import "../styles/main.css";
+import { Link } from "react-router-dom";
 
 const Main = () => {
-    return (
-        <>
-            <form className="textBox" action="" method="get">
-                Prompt :
-                <input type="textarea" className="text1" 
-                placeholder="English please." />
-                <button className="gen">Create
-                </button>
-            </form>
+  return (
+    <>
+      <form className="choice" action="#" method="get">
+        <label> Choice : </label>
+        <select name="chanuilChoice">
+          <option value="cha" selected>character</option>
+          <option value="buil">building</option>
+        </select>
+      </form>
+      <form className="textBox" action="#" method="get">
+        Prompt :
+        <input
+          type="textarea"
+          className="text1"
+          placeholder="English please."
+        />
+        <button className="gen">Create</button>
+      </form>
 
-            <div className="outPut">
+
+
+      <div className="outPut">
                 <Link to="/output">
                 <button className="out">Show me</button>
                 </Link>
             </div>
-
-
-        </>
-    )
-}
+    </>
+  );
+};
 
 export default Main;
